21edff26e65874ad3ad25ecd7542f7608238255e
gjeanmart <greg@jeanmart.me>
2022-02-28 22:41:34 +0100

    add config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger('main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+  const NODE_ENV = configService.get<string>('NODE_ENV');
+
+  await app.listen(PORT);
+  logger.log(
+    `${NODE_ENV}, Sandbox ready on PORT - ${PORT}\n Environment`,
+  );
 }
 bootstrap();
